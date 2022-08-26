99839322556407ec942eb928470e485e05b164a7
tharvik <tharvik@users.noreply.github.com>
2022-08-26 10:32:12 +0200

    dashboards/common: bump alert grace periode

--- dashboards-to-provision/common.py
+++ dashboards-to-provision/common.py
@@ -49,11 +49,11 @@ def simple_graph(
             name=title,
             message=title,
             frequency=f"{frequency}m",
-            gracePeriod=f"{frequency*2}m",
+            gracePeriod=f"{frequency*4}m",
             alertConditions=[
                 AlertCondition(
                     target=Target(refId="A"),
-                    timeRange=TimeRange(f"{frequency*2}m", "now"),
+                    timeRange=TimeRange(f"{frequency*4}m", "now"),
                     evaluator=GreaterThan(alert_at),
                     operator=OP_AND,
                     reducerType=RTYPE_AVG,
